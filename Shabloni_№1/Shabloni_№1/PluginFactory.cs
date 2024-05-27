2f3d7fba14a2f9ba5217ae50acc1909cc94cb1de
Markus <barikal1603@gmail.com>
2024-05-27 19:34:09 +0300

    commit

---
+++ Shabloni_№1/Shabloni_№1/PluginFactory.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using Common;
+
+public static class PluginFactory
+{
+    public static IPlugin GetPlugin(int number)
+    {
+        string[] pluginFiles = Directory.GetFiles(Directory.GetCurrentDirectory() + @"\Plugins\", "*.dll");
+
+        if (number < 1 || number > pluginFiles.Length)
+        {
+            //Console.WriteLine("Invalid plugin number");
+            return null;
+        }
+
+        string pluginFile = pluginFiles[number - 1];
+        string pluginClassName = Path.GetFileNameWithoutExtension(pluginFile);
+
+        Assembly assembly = Assembly.LoadFile(pluginFile);
+        
+        Type type = assembly.GetType(pluginClassName);        
+
+        IPlugin plugin = (IPlugin)Activator.CreateInstance(type);
+
+        return plugin;
+    }
+}
---
+++ Shabloni_№1/Shabloni_№1/Program.cs
@@ -0,0 +1,27 @@
+﻿using Common;
+using System.Reflection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"Сегодня: {DateTime.Today.ToLongDateString()}\n");
+
+        Console.WriteLine("Выберите, какой плагин использовать:");
+
+        START:
+        try
+        {
+            IPlugin plugin = PluginFactory.GetPlugin(int.Parse(Console.ReadLine()));
+
+            plugin.Initialize();
+            plugin.Execute();
+            plugin.Terminate();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("К сожалению, плагин под таким номером недоступен\nПопробуйте другой...\n");
+        }
+        goto START;
+    }
+}
